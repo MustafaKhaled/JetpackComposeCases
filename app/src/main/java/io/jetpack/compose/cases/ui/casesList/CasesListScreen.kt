d918b0ac8dc08277641b19b744459f4d27a47d26
mustafa.khaled <mustafa.khaled@bitpanda.com>
2022-07-13 23:33:52 +0200

    remove SplashScreen module create CasesListScreen.kt

---
+++ app/src/main/java/io/jetpack/compose/cases/ui/casesList/CasesListScreen.kt
@@ -0,0 +1,9 @@
+package io.jetpack.compose.cases.ui.casesList
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavHostController
+
+@Composable
+fun CasesListScreen(navController: NavHostController) {
+
+}
--- app/src/main/java/io/jetpack/compose/cases/ui/navigation/Navigation.kt
+++ app/src/main/java/io/jetpack/compose/cases/ui/navigation/Navigation.kt
@@ -4,6 +4,7 @@ import androidx.compose.runtime.Composable
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import io.jetpack.compose.cases.ui.casesList.CasesListScreen
 import io.jetpack.compose.splashscreen.SplashScreen
 
 /**
@@ -17,6 +18,9 @@ fun Navigation() {
     NavHost(navController = navController, startDestination = NavRoutes.SplashScreen.route) {
         composable(route = NavRoutes.SplashScreen.route) {
             SplashScreen(navController)
+        }
+        composable(route = NavRoutes.CasesListScreen.route) {
+            CasesListScreen(navController)
         }
     }
 }
